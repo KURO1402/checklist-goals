136c4d885721b4ddb794d91964d5e829d475cbf2
PabloDev <yauripablo70@gmail.com>
2026-06-23 21:24:07 -0500

    Pequeño avanze del dasboard

---
+++ view/src/components/ui/MyGoalCard.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    nombre: String,
+    descripcion: String
+})
+</script>
+<template>
+    <div class="border bg-primary p-5 rounded-2xl">
+        <p>{{ nombre }}</p>
+        <p>{{ descripcion }}</p>
+        <div>
+            
+        </div>
+    </div>
+</template>
--- view/src/views/private/Dashboard.vue
+++ view/src/views/private/Dashboard.vue
@@ -1,7 +1,27 @@
 <script setup>
+    import MyGoalCard from "../../components/ui/MyGoalCard.vue";
+    import {ref} from "vue";
+    const objetivos = ref([
+        {
+            id: 1,
+            nombreObjetivo: "Correr",
+            descripcionObjetivo: "Correr diariamente"
+        }
+    ]);
+    const objetivo = ref({
+        id: 0,
+        nombreObjetivo: "",
+        decripcionObjetivo: ""
+    })
 </script>
 <template>
     <div>
-        <h1>Dashboard</h1>
+        <h2 class="text-text font-bold text-2xl">Tus objetivos</h2>
+    </div>
+    <div v-if="objetivos.length === 0">
+        <span>Aun no tienes objetivos</span>
+    </div>
+    <div class="flex">
+        <MyGoalCard v-for="obj in objetivos" :key="obj.id" :nombre="obj.nombreObjetivo" :descripcion="obj.descripcionObjetivo"/>
     </div>
 </template>
